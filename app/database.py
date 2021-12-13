0b68245e00821c9eff6abf16d5e5201c1b407d7c
jtortolero <josegabrieltortolero@gmail.com>
2021-12-13 11:51:44 -0300

    Models and Database

--- app/database.py
+++ app/database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgres://postgres:1234@localhost/fastAPi'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/fastAPi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
